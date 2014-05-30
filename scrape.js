9bd8a77859429613cd439044b81d03abeda38976
Rene <devproren@gmail.com>
2014-05-29 17:36:03 -0700

    need to figure out how to effectively combine all json data from each training program

--- scrape.js
+++ scrape.js
@@ -15,22 +15,57 @@ if ((argv.h) || (argv.help) ){
 if ((argv.out_dir)) out_dir = argv.out; 
 if ((argv.o)) out_dir = argv.o; 
 
-fs.readFile('links.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  links = JSON.parse(data); 
+var dl_all = function(err, data) {
+  fs.readFile('links.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    links = JSON.parse(data); 
 
-  for (var race_type in links){ // race_type is 'full', 'half', etc.
-    race_type_links = links[race_type];
-    for (var training_type in race_type_links) { // training_type is 'novice_1', 'intermediate_2', etc.
-      url_link = race_type_links[training_type];
-      file_name = out_dir + '/' + race_type + '_' + training_type + '.json';
-      create_output_file(url_link, file_name);
+    // download all the json files individually
+    for (var race_type in links) { // race_type is 'full', 'half', etc.
+      var race_type_links = links[race_type];
+      for (var training_type in race_type_links) { // training_type is 'novice_1', 'intermediate_2', etc.
+        var url_link = race_type_links[training_type];
+        var file_name = out_dir + '/' + race_type + '_' + training_type + '.json';
+
+        scrape_table(url_link, (function(file){
+          return function(json){
+            fs.writeFile(file, JSON.stringify(json_table, null, 4), function(err){
+              console.log( file + ' sucessfully written');
+            });         
+          };
+        })(file_name));
+      }
     }
-  }
+  });
+};
 
-});
+//   // combine them into a all.json file
+//   var json_all_training_data = {};
+//   for (var race_type in links) { // race_type is 'full', 'half', etc.
+//     var race_type_links = links[race_type];
+//     json_all_training_data[race_type] = {};
+//
+//     for (var training_type in race_type_links) { // training_type is 'novice_1', 'intermediate_2', etc.
+//       var url_link = race_type_links[training_type];
+//         
+//       var file_name = out_dir + '/' + race_type + '_' + training_type + '.json';
+//       fs.readFile(file_name, 'utf8', function (err,data){
+//         console.log(race_type);
+//         console.log(training_type);
+//         console.log(file_name);
+//         json_all_training_data[race_type][training_type] = JSON.parse(data);
+//       });
+//     }
+//   }
+//
+//   // var file = out_dir + '/' + 'all.json';
+//   // fs.writeFile(file, JSON.stringify(json_all_training_data, null, 4), function(err){
+//   //   console.log( file + ' sucessfully written');
+//   // });         
+// });
 
-var create_output_file = function(url, file_name) {
+// scrape training table given url, hands json to callback function
+var scrape_table = function(url, callback) {
   request(url, function(error, response, html){
     if(!error){
       var $ = cheerio.load(html);
@@ -60,9 +95,44 @@ var create_output_file = function(url, file_name) {
 
       json_table.rows = rows;
 
-      fs.writeFile(file_name, JSON.stringify(json_table, null, 4), function(err){
-        console.log( file_name + ' sucessfully written');
-      });
+      callback(json_table);
     }
   });
 };
+
+var scrape_table_sync = function(url) {
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+
+      json_table = { headers : "", rows : "" }
+
+      rows = []
+      $('.table-training tbody tr').filter(function(i){
+        if(i === 0){
+          table_headers = []
+          $(this).children().each(function(){
+            column_header = $(this).text();
+            table_headers.push(column_header);
+            json_table.headers = table_headers;
+          });
+        } else{
+          json_row = {}
+          $(this).children().each(function(i){
+            cell_data = $(this);
+            column_header = table_headers[i];
+            json_row[column_header] = cell_data.text();
+
+          });
+          rows.push(json_row);
+        }
+      });
+
+      json_table.rows = rows;
+
+      return json_table;
+    }
+  });
+};
+
+dl_all();
