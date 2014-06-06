54996603a9b9308ba0480596b360217f650dd722
Rene <devproren@gmail.com>
2014-06-06 11:24:55 -0700

    added more page event handlers for debugging

--- public/js/main.js
+++ public/js/main.js
@@ -34,7 +34,7 @@ var load_main_page = function() {
     if (json_training === null) {
         var race_type = window.localStorage.getItem('race_type');
         var race_level = window.localStorage.getItem('race_level');
-        fetch_training_json(race_type, race_level, function(data){
+        fetch_training_data(race_type, race_level, function(data){
             update_main_page(data);
             window.localStorage.setObject('json_training', data);
         });
@@ -43,25 +43,35 @@ var load_main_page = function() {
     }
  }
 
+// Page is the page element id. See load_train_plan_page()
+var load_select_options = function(page, training_list, race_type, race_level) {
+    if (race_type === undefined) {
+        //TODO change to default to first type of race. will need to change json format given by server.
+        race_type = 'full'; 
+    }
+
+    var race_options = gen_race_options(training_list, race_type);
+    var level_options = gen_level_options(training_list, race_type, race_level);
+
+    var select_race = $(page).find('select.select-race-type');
+    var select_level = $(page).find('select.select-race-level');
+
+    select_race.append(race_options);
+    select_level.append(level_options);
+}
+
 var load_race_date_page = function() {
     var race_date = window.localStorage.getItem('race_date');
-    $('#date-input').val(race_date).trigger('change');
-}
-
-var load_train_plan_page = function() {
-
-}
-
-var update_train_plan_page = function(race_options, level_options) {
-
+    console.log(race_date);
+    $('#date-input').val(race_date);
 }
 
 var save_race_date_page = function() {
     var old_race_date = window.localStorage.getItem('race_date');
     var new_race_date = $('#date-input').val();
 
-    console.log(old_race_date);
-    console.log(new_race_date);
+    console.log('old ' + old_race_date);
+    console.log('new ' + new_race_date);
 
     if (old_race_date !== new_race_date) {
         window.localStorage.setItem('race_date', new_race_date);
@@ -73,8 +83,6 @@ var save_train_plan_page = function() {
     var race_type = $('#train-plan').find('select.select-race-type').val();
     var race_level = $('#train-plan').find('select.select-race-level').val();
 
-    console.log(race_type);
-    console.log(race_level);
 
     var saved_race_type = window.localStorage.getItem('race_type');
     var saved_race_level = window.localStorage.getItem('race_level');
@@ -91,18 +99,17 @@ var save_train_plan_page = function() {
 
  
 
-var fetch_training_json = function(race_type, race_level, callback) {
+var fetch_training_data = function(race_type, race_level, callback) {
     var req_url = 'api?race=' + race_type + '&level=' + race_level;
     $.getJSON(req_url, callback).fail(function(problem){
         console.log(problem.responseText);
     });
 }
 
-var fetch_option_vals = function() {
+var fetch_training_list = function(callback) {
     req_url = 'api/training_list';
-    $.getJSON(req_url, function(data){
-        console.log(data);
-        window.localStorage.setObject('option_vals', data);
+    $.getJSON(req_url, callback).fail(function(problem){
+        console.log(problem.responseText);
     });
 }
 
@@ -113,41 +120,77 @@ var to_title = function(str){
 
 var day_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-fetch_option_vals();
 
 // Page event bindings
-// Function bindings on 'pagebeforecreate' are only bound ONCE, when the page is first created.
+// Function bindings on 'pagebeforecreate' are only bound ONCE since pagebeforecreate is only fired once for each page.
 
 // If visiting app for first time, change page to #init-setup
+
 $('#main').on('pagebeforecreate', function(e) {
-    var race_date = window.localStorage.getItem('race_date');
-    if (race_date === null) {
-        $( ":mobile-pagecontainer" ).pagecontainer( "change", "#init-setup" );
-    }
-    load_main_page();
-});
 
-// Save 'run type' and 'level' upon exiting #train-plan page.
-$('#train-plan').on('pagebeforecreate', function(){
+    // App starts here
+    // First fetch training list from server.    
+    fetch_training_list(function(data){
+        window.localStorage.setObject('training_list', data);
 
-    // Once select boxes change in value, save values to localStorage
-    // 'change' event will propogate up from select boxes
-    $(this).on('change', function(e){
-        save_train_plan_page(); 
+        var race_date = window.localStorage.getItem('race_date');
+        var race_type = window.localStorage.getItem('race_type');
+        var race_level = window.localStorage.getItem('race_level');
+
+        if ( !(race_date && race_type && race_level) ) {
+            $( ":mobile-pagecontainer" ).pagecontainer( "change", "#init-setup" );
+        } else {
+            load_main_page();
+        }
     });
 
 });
 
+$('#train-plan').on('pagebeforecreate', function(){
+    
+    // Load select boxes with race type and level that user should have 
+    // set in #init-setup
+    var training_list = window.localStorage.getObject('training_list');
+    var race_type = window.localStorage.getItem('race_type');
+    var race_level = window.localStorage.getItem('race_level');
+
+    load_select_options(this, training_list, race_type, race_level) 
+
+    // Save 'run type' and 'level' upon exiting #train-plan page.
+    // Once select boxes change in value, save values to localStorage.
+    // 'change' event will propogate up from changing select menus.
+    $(this).on('change', function(e){
+        save_train_plan_page(); 
+    });
+    
+});
+
 $('#race-date').on('pagebeforecreate', function(){
 
+    var race_date = window.localStorage.getItem('race_date');
+    console.log($('#date-input'));
+    console.log('val: ' + $('#date-input').val());
+
+    // When date input changes, save the changed value to localStorage.
+    //
+    // Date value will persist, so no need to reload it when visiting race-date 
+    // page again later.
     $(this).on('change', function(e){
         save_race_date_page(); 
     });
 
 });
 
-// Save settings after completing init-settings page.
+
+// Event bindings for #init-setup page
 $('#init-setup').on('pagebeforecreate', function(e) {
+
+    var training_list = window.localStorage.getObject('training_list');
+
+    console.log('training list not ready?');
+    load_select_options(this, training_list);
+
+    // Save settings after completing init-settings page.
     $('#save-init-settings').on('click', function(){
         var input_date = $('#init-date-input');
         window.localStorage.setItem('race_date', input_date.val());
@@ -165,73 +208,138 @@ $('#init-setup').on('pagebeforecreate', function(e) {
 
 // Page transition events
 $(document).on('pagecontainerbeforetransition', function(e, ui) {
+    console.log('pcbeforetransition');
     switch (ui.toPage[0].id) {
         case 'main':
             load_main_page();
         case 'race-date':
+
             load_race_date_page();
         case 'train-plan':
-            load_train_plan_page();
+            // load_train_plan_page();
     } 
 });
 
-$(document).on('pagecontainerbeforeshow', function(e, ui) {
+$(document).on('pagecontainerbeforeload', function(e, ui) {
+    console.log('beforeload');
+    console.log('val: ' + $('#date-input').val());
+
     if (ui.prevPage[0]) {
         switch(ui.prevPage[0].id) {
         }
     }
 });
 
+$(document).on('pagecontainerload', function(e, ui) {
+    console.log('load');
+    console.log('val: ' + $('#date-input').val());
 
-$(document).on('pagebeforecreate', function(){
+    if (ui.prevPage[0]) {
+        switch(ui.prevPage[0].id) {
+        }
+    }
+});
+
+$(document).on('pagecontainereshow', function(e, ui) {
+    console.log('show');
+    console.log('val: ' + $('#date-input').val());
+
+    if (ui.prevPage[0]) {
+        switch(ui.prevPage[0].id) {
+        }
+    }
+});
+
+$(document).on('pagecontainerbeforeshow', function(e, ui) {
+    console.log('beforeshow');
+    // console.log(ui);
+    // console.log('val: ' + $('#date-input').val());
+    //
+    //
+    console.log('val: ' + $('#date-input').val());
+    $('#date-input').trigger('change');
+
+    if (ui.prevPage[0]) {
+        switch(ui.prevPage[0].id) {
+        }
+    }
+});
+
+// race_type optional
+var gen_race_options = function(training_list, race_type){
+    var option_elements = [];
+    var i = 0;
+    // TODO make sure training_list is an array of objects in the future.
+    // Right now, for loop on an object such as training_list is incorrect 
+    // since it has no concept of an index.
+    //
+    // We need index to set the first option to selected if no race_type 
+    // is specified.
+    for (var type in training_list) {
+        var option_tag = $('<option/>', {
+                    value:type,
+                    text: to_title(type),
+                });
+        if (race_type !== null ) {
+            if (race_type === type) {
+                option_tag.attr('selected', 'selected');
+            }
+        } else if (i === 0){
+            option_tag.attr('selected', 'selected');
+        }
+        option_elements.push(option_tag);
+        i = i + 1;
+    }
+    return option_elements;
+}
+
+// race_level optional
+var gen_level_options = function(training_list, race_type, race_level) {
+    var level_options = training_list[race_type];
+    var option_elements = [];
+    $.each(level_options, function(i, level){
+        var option_tag = $('<option/>', {
+                    value:level,
+                    text: to_title(level),
+                });
+        if (race_level !== null) {
+            if (race_level === level) {
+                option_tag.attr('selected', 'selected');
+            }
+        } else if (i === 0) {
+            option_tag.attr('selected', 'selected');
+        } 
+        option_elements.push(option_tag);
+    });
+
+    return option_elements;
+}
+
+// TODO if training_plan or training_data is updated on the server, will have to fetch the data.
+// If the data is changed on server, we don't have to immediately push it out to
+// client.  Just have the client poll for changes every time app is reloaded (refreshed).
+
+// App should request training_plan first, and then training_data.
+
+$(document).on('pagebeforecreate', function(e){
     // init external panel
-    $('[data-role=panel]').panel().enhanceWithin();
+    
 
     // Add select handler to all .select-race-type in app.
-    // TODO refactor to put 'options processing' logic into its own function/elsewhere
-    $('select.select-race-type').change(function(e){
+    // Changes level select options based on currently selected race type.
+    $('select.select-race-type').on('change', function(e){
         var level_select = $(this).closest('[data-role=content]').find('select.select-race-level');
         level_select.empty();
 
         var race_type = $(this).val();
-        var option_vals = window.localStorage.getObject('option_vals');
+        var training_list = window.localStorage.getObject('training_list');
 
-        if (option_vals !== null) {
-            var level_options = option_vals[race_type];
-            $.each(level_options, function(i, level){
-                var option_tag = $('<option/>', {
-                            value:level,
-                            text: to_title(level),
-                        });
-                if (i === 0) {
-                    option_tag.attr('selected', 'selected');
-                } 
-                level_select.append(option_tag);
-            });
-            level_select.selectmenu('refresh', true);
-        } else {
-            console.log('Did not receive option_vals from server');
-        }
+        var option_tags = gen_level_options(training_list, race_type);
+        $.each(option_tags, function(i, tag){
+            level_select.append(tag);
+        });
+        level_select.selectmenu('refresh', true);
     });
-
-    
-    // Save race date from race date page
-
-    
-
-
-    // // Page transitions
-    // $(document).on('pagebeforechange', function(e,ui){
-    //     var race_date = window.localStorage.getItem('race_date');
-    //     console.log(e);
-    //     if (race_date !== null) {
-    //         //redirect to main page
-    //         window.location.replace(window.location.origin + '/#main');
-    //     }
-    // });
-
-
-    
 
 
     //TODO add event handlers for 'pagecontainershow' to set race day, run type, and run level values for their respective pages.
@@ -248,6 +356,7 @@ $(document).on('pagebeforecreate', function(){
 });
 
 
-
-
-
+// TODO fix main panel loading issue
+$(function() {
+    $('[data-role=panel]').panel().enhanceWithin();
+});
